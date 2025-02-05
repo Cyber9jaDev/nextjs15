dd31fb2c7f460a8541272681412a0f4f232f253a
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-05 15:25:54 +0000

    action state

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,4 +1,11 @@
+"use client";
+
+import Link from "next/link";
+import { useActionState } from "react";
+
 export default function Register() {
+  const [ state, formAction, isPending ] = useActionState(register, undefined);
+  
   return (
     <div className="container w-1/2">
       <h1 className="title">Register</h1>
@@ -14,9 +21,15 @@ export default function Register() {
         </div>
         <div>
           <label htmlFor="email">Confirm Password</label>
-          <input type="text" name="" />
+          <input type="text" name="confirmPassword" />
+        </div>
+
+        <div className="flex items-end gap-4">
+          <button className="btn-primary">Register</button>
+          <Link href="/" className="text-link">or login here</Link>
         </div>
       </form>
     </div>
   );
+
 }

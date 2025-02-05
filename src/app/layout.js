938d76f1783b5e7b66feedd0bab3d7405e332a98
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-05 14:01:32 +0000

    minor fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,16 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body> {children} </body>
+      <body> 
+        <header>
+          <nav>Nav</nav>
+        </header>
+
+        <main> {children} </main>
+
+        <footer>Footer</footer>
+      
+      </body>
     </html>
   );
 }

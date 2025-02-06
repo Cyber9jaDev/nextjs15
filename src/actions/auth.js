5f76ae1c7295cdf33740233f7bdc447b3e509101
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-06 08:27:56 +0000

    validation-zod

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,22 +1,31 @@
 "use server";
 
+import { RegisterFormSchema } from "@/lib/rules";
+
 // All the functions must run on the server side
 // All the functions must be async
 // All the functions must be named with "use server"
 
 export async function register(previousState, formData){
 
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const confirmPassword = formData.get("confirmPassword");
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  console.log(email);
-  console.log(password);
-  console.log(confirmPassword);
-}
+  // const email = formData.get("email");
+  // const password = formData.get("password");
+  // const confirmPassword = formData.get("confirmPassword");
 
-export async function login(previousState, formData){
-  console.log("Login action called");
+  const validatedFields = RegisterFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      email: formData.get("email"),
+    };
+  }
+
+  console.log(validatedFields);
 }

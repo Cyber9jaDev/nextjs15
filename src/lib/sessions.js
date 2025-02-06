66d6897bc7f59697efced12be3c0758850ee4a67
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-06 12:17:53 +0000

    minor fix

--- src/lib/sessions.js
+++ src/lib/sessions.js
@@ -1,6 +1,7 @@
 import 'server-only';
 
 import { SignJWT, jwtVerify } from 'jose'
+import { cookies } from 'next/headers';
 
 const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
@@ -20,4 +21,18 @@ export async function decrypt(session) {
   } catch (error) {
     console.log('Failed to verify session');
   }
+}
+
+export async function createSession(userId) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const session = await encrypt({ userId, expiresAt });
+  const cookieStore = await cookies();
+
+  cookieStore.set('session', session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: 'lax',
+    path: '/',
+  })
 }

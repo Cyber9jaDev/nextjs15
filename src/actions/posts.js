e311525f361ead03013c26940cdcffa6afb4b448
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-07 11:51:00 +0000

    minor fix

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { getCollection } from "@/lib/db";
 import { getAuthUser } from "@/lib/getAuthUser";
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,29 @@
-export default function Home() {
+import { getCollection } from "@/lib/db";
+import Link from "next/link";
 
-  return (
-    <div>
-      <h1>Home Page </h1>
-    </div>
-  );
+export default async function Home() {
+  const postsCollection = await getCollection("Posts");
+  const posts = await postsCollection.find({}).toArray();
+  if(posts){
+    return (
+      <div className="grid grid-cols-2 gap-6">
+        {
+          posts.map(post => {
+            return(
+              <div key={post._id}>
+                <div className="border border-slate-400 border-dashed p-4 rounded-md h-full">
+                  <p className="text-slate-400 text-xs">Created at</p>
+                  <Link href="" className="block text-xl font-semibold mb-4">{post.title}</Link>
+                  <p className="text-sm">{post.content}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
+  else return <p>Failed to fetch the data from database</p>
+
+  
 }

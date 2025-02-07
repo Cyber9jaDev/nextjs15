c8b460249503eb40cce6fd3b95227f1c7e6fcf7e
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-07 11:59:00 +0000

    crud-read

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,17 @@
+import PostCard from "@/components/PostCard";
 import { getCollection } from "@/lib/db";
-import Link from "next/link";
 
 export default async function Home() {
   const postsCollection = await getCollection("posts");
-  const posts = await postsCollection.find({}).toArray();
+  const posts = await postsCollection.find({}).sort({ $natural: -1 }).toArray();
 
   if(posts){
     return (
       <div className="grid grid-cols-2 gap-6">
-        {
-          posts.map(post => {
+        { posts.map(post => {
             return(
               <div key={post._id}>
-                <div className="border border-slate-400 border-dashed p-4 rounded-md h-full">
-                  <p className="text-slate-400 text-xs">Created at</p>
-                  <Link href="" className="block text-xl font-semibold mb-4">{post.title}</Link>
-                  <p className="text-sm">{post.content}</p>
-                </div>
+                <PostCard post={post} />
               </div>
             )
           })
